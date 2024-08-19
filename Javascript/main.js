52cf501946c67ac3534589e133b49fbbe65a098d
Uppkabinjay <uppkabintarajaya@gmail.com>
2024-08-19 20:34:44 +0700

    Add files via upload

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.btnDetail').forEach(item=>{
+document.querySelectorAll('.btnDetail1').forEach(item=>{
     item.addEventListener('click',(e)=>{
         let parent = e.target.parentNode.parentNode;
 
@@ -20,8 +20,95 @@ document.querySelectorAll('.btnDetail').forEach(item=>{
         document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
         document.querySelector('.modalHarga').innerHTML = harga;
 
-        const nohp = '+628987871911';
-        let pesan = `https://api.whatsapp.com/send?phone=${628112003991}&text=Haloo, saya mau pesan produk tersebut ${gambar}`;
+        const nohp = '6281281285738';
+        let pesan = `https://api.whatsapp.com/send?phone=${6281281285738}&text=Haloo, saya mau pesan produk tersebut ${gambar}`;
+
+        document.querySelector('.btnBeli').href = pesan;
+    });
+});
+
+document.querySelectorAll('.btnDetail2').forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi')? parent.querySelector('.deskripsi').innerHTML:'<i>tidak ada<i>';
+        
+
+        let tombolModal = document.querySelector('.btnModal');
+        tombolModal.click();
+
+        document.querySelector('.modalTitle').innerHTML = judul;
+        let image = document.createElement('img');
+        image.src = gambar;
+        image.classList.add('w-100');
+        document.querySelector('.modalImage').innerHTML = '';
+        document.querySelector('.modalImage').appendChild(image);
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+        document.querySelector('.modalHarga').innerHTML = harga;
+
+        const nohp = '6287775514102';
+        let pesan = `https://api.whatsapp.com/send?phone=${6287775514102}&text=Haloo, saya mau pesan produk tersebut ${gambar}`;
+
+        document.querySelector('.btnBeli').href = pesan;
+    });
+});
+
+document.querySelectorAll('.btnDetail3').forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi')? parent.querySelector('.deskripsi').innerHTML:'<i>tidak ada<i>';
+        
+
+        let tombolModal = document.querySelector('.btnModal');
+        tombolModal.click();
+
+        document.querySelector('.modalTitle').innerHTML = judul;
+        let image = document.createElement('img');
+        image.src = gambar;
+        image.classList.add('w-100');
+        document.querySelector('.modalImage').innerHTML = '';
+        document.querySelector('.modalImage').appendChild(image);
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+        document.querySelector('.modalHarga').innerHTML = harga;
+
+        const nohp = '6285718010161';
+        let pesan = `https://api.whatsapp.com/send?phone=${6285718010161}&text=Haloo, saya mau pesan produk tersebut ${gambar}`;
+
+        document.querySelector('.btnBeli').href = pesan;
+    });
+});
+
+document.querySelectorAll('.btnDetail4').forEach(item=>{
+    item.addEventListener('click',(e)=>{
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi')? parent.querySelector('.deskripsi').innerHTML:'<i>tidak ada<i>';
+        
+
+        let tombolModal = document.querySelector('.btnModal');
+        tombolModal.click();
+
+        document.querySelector('.modalTitle').innerHTML = judul;
+        let image = document.createElement('img');
+        image.src = gambar;
+        image.classList.add('w-100');
+        document.querySelector('.modalImage').innerHTML = '';
+        document.querySelector('.modalImage').appendChild(image);
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+        document.querySelector('.modalHarga').innerHTML = harga;
+
+        const nohp = '6285718010161';
+        let pesan = `https://api.whatsapp.com/send?phone=${6285718010161}&text=Haloo, saya mau pesan produk tersebut ${gambar}`;
 
         document.querySelector('.btnBeli').href = pesan;
     });
